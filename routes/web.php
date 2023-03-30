d91c678bdb9c35ab89edc8ddbf8e850f8b73ca85
Lifa Matemba <lmatemba5@gmail.com>
2023-03-30 09:24:31 +0200

    Added Heroicon package

--- routes/web.php
+++ routes/web.php
@@ -25,6 +25,10 @@ Route::get('/', function () {
     ]);
 });
 
+Route::get('/navbar', function () {
+    return Inertia::render('Navbar');
+});
+
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
